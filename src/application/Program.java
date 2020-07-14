83b7000c07567bfaafc39f080cefa21c5ca32fd5
Joao Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2020-07-14 10:59:51 -0300

    Versão ruim

---
+++ src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.print("Número do quarto: ");
+		int numeroQuarto = sc.nextInt();
+		
+		System.out.print("Data de check-in (DD/MM/AAAA): ");
+		Date dataCheckIn = sdf.parse(sc.next());
+		
+		System.out.print("Data de check-out (DD/MM/AAAA): ");
+		Date dataCheckOut = sdf.parse(sc.next());
+		
+		if (!dataCheckOut.after(dataCheckIn)) {
+			System.out.println("Erro na reserva: Data de Check-Out deve ser depois da data de Check-In");
+		} else {
+			Reservation reserva = new Reservation(numeroQuarto, dataCheckIn, dataCheckOut);
+			System.out.println("Reservas: " + reserva);
+			System.out.println();
+			
+			System.out.println("Entre com as informações para atualizar a reserva: ");
+			System.out.print("Data de check-in (DD/MM/AAAA): ");
+			dataCheckIn = sdf.parse(sc.next());
+			
+			System.out.print("Data de check-out (DD/MM/AAAA): ");
+			dataCheckOut = sdf.parse(sc.next());
+			
+			Date agora = new Date();
+			if (dataCheckIn.before(agora) || dataCheckOut.before(agora)) {
+				System.out.println("Erro na reserva: As datas para atualizar a reserva devem ser datas futuras!");
+			} else if (!dataCheckOut.after(dataCheckIn)){
+				System.out.println("Erro na reserva: Data de Check-Out deve ser depois da data de Check-In");
+			} else {
+				reserva.atualizaData(dataCheckIn, dataCheckOut);
+				
+				System.out.println("Reservas atualizadas: " + reserva);
+			}
+		}
+		sc.close();
+	}
+
+}
---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,63 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	private Integer numeroQuarto;
+	private Date checkIn;
+	private Date checkOut;
+	
+	public Reservation() {
+	}
+
+	public Reservation(Integer numeroQuarto, Date checkIn, Date checkOut) {
+		this.numeroQuarto = numeroQuarto;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+	public void setNumeroQuarto(Integer numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duracao() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void atualizaData(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString () {
+		return "Quarto "
+			+ numeroQuarto
+			+ ", Check-In: "
+			+ sdf.format(checkIn)
+			+ ", Check-Out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duracao()
+			+ " noites";
+	}
+	
+}
